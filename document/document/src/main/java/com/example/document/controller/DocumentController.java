15c862d0b446a35c4a566279de425661f81e2764
aongtosan <aongtosan@gmail.com>
2023-06-01 08:57:39 +0700

    add pom.xml finalname=document

--- document/document/src/main/java/com/example/document/controller/DocumentController.java
+++ document/document/src/main/java/com/example/document/controller/DocumentController.java
@@ -1,14 +1,16 @@
 package com.example.document.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/document")
 public class DocumentController {
+    private final Logger logger = LoggerFactory.getLogger(DocumentController.class);
     @GetMapping(path = "/{documentName}", produces = "application/json")
-
     public String findDocument(@PathVariable(required=false,name="documentName") String documentName){
-
+        logger.info("/document/"+documentName);
         return "Here your document id : "+documentName;
     }
 }
