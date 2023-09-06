a1ea786a1cd8b0f4f62a0f9a30dd95cc2610236b
aongtosan <aongtosan@gmail.com>
2023-09-06 20:53:04 +0700

    update structure project log4j

--- document/document/src/main/java/com/example/document/controller/DocumentController.java
+++ document/document/src/main/java/com/example/document/controller/DocumentController.java
@@ -1,13 +1,13 @@
 package com.example.document.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/document")
 public class DocumentController {
-    private final Logger logger = LoggerFactory.getLogger(DocumentController.class);
+    private final Logger logger = LogManager.getLogger(DocumentController.class);
     @GetMapping(path = "/{documentName}", produces = "application/json")
     public String findDocument(@PathVariable(required=false,name="documentName") String documentName){
         logger.info("METHOD:GET endpoint => /document/"+documentName);
